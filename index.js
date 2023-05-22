060ce938dd3ecc11dd7a26600be6c57d7e34d96a
Ellian Rodrigues <ellian.rodrigues.ti@gmail.com>
2023-05-22 13:48:13 -0300

    voltou

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ app.get('/', function (req, res) {
   res.send('Hello World! Teste API RailWay')
 })
 
-app.listen(21262,()=>{
-    console.log('Express started at http://localhost:21262')
-})
+app.listen(process.env.PORT || 3000)
